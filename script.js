8fbeb7d8c362a3c12b526b3c2e4576d760628b3e
RosaleeKnight <rtknight92@outlook.com>
2022-03-04 08:46:29 -0600

    Edit script.js

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperationTextElement = document.querySelector('[data-previous-operation]')
 const currentOperationTextElement = document.querySelector('[data-current-operation')
+
+class Calculator {
+    constructor(previousOperationTextElement, currentOperationTextElement) {
+        this.previousOperationTextElement = previousOperationTextElement
+        this.currentOperationTextElement = currentOperationTextElement
+        this.clear ()
+    }
+}
+
+clear () {
+    this.currentOperation = ''
+    this.previousOperation = ''
+    this.operation = undefined
+}
+
+const calculator = new Calculator(previousOperationTextElement, currentOperationTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
