5f0d26bc2e8c93419abc64e69cd1d12b8dae3d8d
RosaleeKnight <rtknight92@outlook.com>
2022-03-04 08:58:54 -0600

    Change order script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+class Calculator {
+    constructor(previousOperationTextElement, currentOperationTextElement) {
+        this.previousOperationTextElement = previousOperationTextElement
+        this.currentOperationTextElement = currentOperationTextElement
+        this.clear ()
+    }
+}
+clear () {
+    this.currentOperation = ''
+    this.previousOperation = ''
+    this.operation = undefined
+}
+delete() {
+    this.currentOperation = this.currentOperation.toString().slice(0,-1)
+}
+appendNumber(number) {
+    if (number === '.' && this.currentOperation.includes('.')) return
+    this.currentOperation = this.currentOperation.toString() + number.toString ()
+}
+chooseOperation(operation) {
+    if (this.currentOperation === '') return
+    if (this.previousOperation !== '') {
+        this.compute()
+    }
+    this.operation = operation
+    this.previousOperation = this.currentOperation
+    this.currentOperation = ''
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -6,25 +36,17 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperationTextElement = document.querySelector('[data-previous-operation]')
 const currentOperationTextElement = document.querySelector('[data-current-operation')
 
-class Calculator {
-    constructor(previousOperationTextElement, currentOperationTextElement) {
-        this.previousOperationTextElement = previousOperationTextElement
-        this.currentOperationTextElement = currentOperationTextElement
-        this.clear ()
-    }
-}
-
-clear () {
-    this.currentOperation = ''
-    this.previousOperation = ''
-    this.operation = undefined
-}
-
 const calculator = new Calculator(previousOperationTextElement, currentOperationTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
 })
